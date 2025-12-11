b043b710f7bffe049183a80ccb8d9bebd71f345b
Johana <112355982+johamc@users.noreply.github.com>
2025-12-11 16:53:28 -0500

    Delete src/explicacion/EjercicioClase.java

--- src/explicacion/EjercicioClase.java
+++
@@ -1,9 +0,0 @@
-package explicacion;
-
-public class EjercicioClase { //clase
- public static void ejercicio1(){//metodo
-     System.out.println("estoy desdes el metodo del ejercicio1");
-     int suma = 4 + 6;
-     System.out.println(suma);
- }
-}
